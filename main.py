a1d6289d6a044aed69215dcbcf535b94b44bc4b1
Nilesh Ghule <nilesh@sunbeaminfo.com>
2025-06-02 16:01:49 +0530

    calc - basic app skeleton

---
+++ main.py
@@ -0,0 +1,11 @@
+
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+
+while True:
+    print("0. exit")
+    choice = int(input("enter choice: "))
+
+    if(choice == 0):
+        print("bye!")
+        exit(0)
