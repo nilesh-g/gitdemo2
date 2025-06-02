65bd0f5a30f16fadf151cf051db77e030e7ffb93
Nilesh Ghule <nilesh@sunbeaminfo.com>
2025-06-02 16:04:09 +0530

    calc - addition

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ num2 = int(input("enter num2: "))
 
 while True:
     print("0. exit")
+    print("1. addition")
     choice = int(input("enter choice: "))
 
     if(choice == 0):
         print("bye!")
         exit(0)
+    elif(choice == 1):
+        res = num1 + num2
+        print("addition:", res)
