1a1a914fe81352da9f46cea8c927b094972aaa49
Nilesh Ghule <nilesh@sunbeaminfo.com>
2025-06-02 16:05:48 +0530

    calc - subtraction

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ num2 = int(input("enter num2: "))
 while True:
     print("0. exit")
     print("1. addition")
+    print("2. subtraction")
     choice = int(input("enter choice: "))
 
     if(choice == 0):
@@ -13,3 +14,6 @@ while True:
     elif(choice == 1):
         res = num1 + num2
         print("addition:", res)
+    elif(choice == 2):
+        res = num1 - num2
+        print("subtraction:", res)
